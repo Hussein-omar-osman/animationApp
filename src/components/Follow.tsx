27f22d00b8add2726f2fcae06234a4510e4172fd
Hussein Omar <husseinomar6190@gmail.com>
2022-11-30 20:36:47 +0300

    add: started follow animation

---
+++ src/components/Follow.tsx
@@ -0,0 +1,27 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const Follow = () => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.circle} />
+    </View>
+  );
+};
+
+export default Follow;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  circle: {
+    height: 80,
+    aspectRatio: 1,
+    backgroundColor: 'blue',
+    borderRadius: 40,
+    opacity: 0.8,
+  },
+});
