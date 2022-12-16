cd1e12e388078249758379004597878b3d99e0ac
Hussein Omar <husseinomar6190@gmail.com>
2022-12-16 19:08:45 +0300

    created boiler plate code for ListAnime

--- src/pages/ListAnime.tsx
+++ src/pages/ListAnime.tsx
@@ -1,14 +1,29 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {FlatList, StyleSheet, View} from 'react-native';
 import React from 'react';
 
 const ListAnime = () => {
+  const data = new Array(3).fill(0).map((_, i) => ({
+    id: i,
+  }));
   return (
-    <View>
-      <Text>ListAnime</Text>
+    <View style={styles.container}>
+      <FlatList data={data} renderItem={() => <View style={styles.sList} />} />
     </View>
   );
 };
 
 export default ListAnime;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#fff',
+  },
+  sList: {
+    height: 60,
+    width: '90%',
+    backgroundColor: 'red',
+  },
+});
