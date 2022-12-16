fdc909d4c2b6f86da4495b6cc8b651026a36f750
Hussein Omar <husseinomar6190@gmail.com>
2022-12-16 16:54:49 +0300

    created bouncing list animation page

---
+++ src/pages/ListAnime.tsx
@@ -0,0 +1,14 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const ListAnime = () => {
+  return (
+    <View>
+      <Text>ListAnime</Text>
+    </View>
+  );
+};
+
+export default ListAnime;
+
+const styles = StyleSheet.create({});
