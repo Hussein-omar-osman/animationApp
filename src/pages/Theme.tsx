0402e8e32017f0daf68b2c64c7c07b62fc247f55
Hussein Omar <husseinomar6190@gmail.com>
2022-12-25 15:15:46 +0300

    update: finished theme animation

--- src/pages/Theme.tsx
+++ src/pages/Theme.tsx
@@ -1,10 +1,10 @@
-import {StyleSheet, Switch, Text, View} from 'react-native';
+import {Dimensions, StatusBar, StyleSheet, Switch} from 'react-native';
 import React, {useState} from 'react';
 import Animated, {
   interpolateColor,
   useAnimatedStyle,
   useDerivedValue,
-  useSharedValue,
+  withTiming,
 } from 'react-native-reanimated';
 
 const Colors = {
@@ -30,7 +30,7 @@ type ThemeType = 'light' | 'dark';
 const Theme = () => {
   const [themeState, setThemeState] = useState<ThemeType>('light');
   const progress = useDerivedValue(
-    () => (themeState === 'light' ? 0 : 1),
+    () => (themeState === 'dark' ? withTiming(1) : withTiming(0)),
     [themeState],
   );
 
@@ -42,22 +42,44 @@ const Theme = () => {
     );
     return {backgroundColor};
   });
+  const rCircleStyle = useAnimatedStyle(() => {
+    const backgroundColor = interpolateColor(
+      progress.value,
+      [0, 1],
+      [Colors.light.circle, Colors.dark.circle],
+    );
+    return {backgroundColor};
+  });
+  const rTextStyle = useAnimatedStyle(() => {
+    const color = interpolateColor(
+      progress.value,
+      [0, 1],
+      [Colors.light.text, Colors.dark.text],
+    );
+    return {color};
+  });
 
   return (
     <Animated.View style={[styles.container, rStyle]}>
-      <Switch
-        value={themeState === 'dark'}
-        onValueChange={toggled => {
-          setThemeState(toggled ? 'dark' : 'light');
-        }}
-        trackColor={SWITCH_TRACK_COLOR}
-        thumbColor={'violet'}
+      <Animated.Text style={[styles.text, rTextStyle]}>Theme</Animated.Text>
+      <Animated.View style={[styles.circle, rCircleStyle]}>
+        <Switch
+          value={themeState === 'dark'}
+          onValueChange={toggled => {
+            setThemeState(toggled ? 'dark' : 'light');
+          }}
+          trackColor={SWITCH_TRACK_COLOR}
+          thumbColor={'violet'}
+        />
+      </Animated.View>
+      <StatusBar
+        barStyle={themeState === 'light' ? 'dark-content' : 'light-content'}
       />
     </Animated.View>
   );
 };
 
-export default Theme;
+const SIZE = Dimensions.get('window').width * 0.7;
 
 const styles = StyleSheet.create({
   container: {
@@ -66,4 +88,28 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  circle: {
+    width: SIZE,
+    height: SIZE,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: SIZE / 2,
+    shadowOffset: {
+      width: 0,
+      height: 20,
+    },
+    shadowOpacity: 0.1,
+    shadowRadius: 10,
+    elevation: 8,
+  },
+  text: {
+    textTransform: 'uppercase',
+    fontSize: 60,
+    fontWeight: '700',
+    letterSpacing: 14,
+    marginBottom: 35,
+  },
 });
+
+export default Theme;
